52c13e6daf858b432ef238ae985b230c2091c526
piyushkarmhe <karmhepiyush@gmail.com>
2022-09-21 23:12:56 +0530

    connected with py

--- index.js
+++ index.js
@@ -52,36 +52,32 @@ app.get(`/about.html`,(req,res)=>{
 app.post(`/test.html/pred`,(req,res)=>{
     const spawn = require('child_process').spawn;
 
-    // Initialise the data
     const data = {
         array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
 
-    // We need to stringify the data as
-    // python cannot directly read JSON
-    // as command line argument.
     let stringifiedData = JSON.stringify(data);
 
-    // Call the python process and pass the
-    // data as command line argument.
-    const py = spawn('python', ['arraysum.py', stringifiedData]);
+    const py = spawn('python', ['Python/test.py', stringifiedData]);
 
     resultString = '';
-
-    // As the stdout data stream is chunked,
-    // we need to concat all the chunks.
+    console.log("here1");
     py.stdout.on('data', function (stdData) {
+        console.log(resultString);
         resultString += stdData.toString();
     });
 
+    console.log("here2");
     py.stdout.on('end', function () {
-        // Parse the string as JSON when stdout
-        // data stream ends
+
+        console.log(resultString);
         let resultData = JSON.parse(resultString);
 
         let sum = resultData['sum'];
         console.log('Sum of array from Python process =', sum);
     });
+
+    return res.send(JSON.stringify({"Posted":"Check Terminal"}));
 });
 
 
