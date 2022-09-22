32ed036b490680dbc1610de10c8843faaaaea9cd
piyushkarmhe <karmhepiyush@gmail.com>
2022-09-23 01:01:02 +0530

    sentimental model connected

--- index.js
+++ index.js
@@ -68,32 +68,25 @@ app.get(`/result.html`,(req,res)=>{
 app.post(`/test.html/pred`,(req,res)=>{
     const spawn = require('child_process').spawn;
 
-    const data = {
-        array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    }
+    // const data = {
+    //     "text": `Ex Wife Threatening SuicideRecently I left my wife for good because she has cheated on me twice and lied to me so much that I have decided to refuse to go back to her. As of a few days ago, she began threatening suicide. I have tirelessly spent these paat few days talking her out of it and she keeps hesitating because she wants to believe I'll come back. I know a lot of people will threaten this in order to get their way, but what happens if she really does? What do I do and how am I supposed to handle her death on my hands? I still love my wife but I cannot deal with getting cheated on again and constantly feeling insecure. I'm worried today may be the day she does it and I hope so much it doesn't happen.`
+    // }
+
+    const data = req.body
 
     let stringifiedData = JSON.stringify(data);
 
-    const py = spawn('python', ['Python/test.py', stringifiedData]);
+    const py = spawn('python', ['Python/suicide.py', stringifiedData]);
 
     resultString = '';
-    console.log("here1");
     py.stdout.on('data', function (stdData) {
-        console.log(resultString);
         resultString += stdData.toString();
     });
 
-    console.log("here2");
     py.stdout.on('end', function () {
-
-        console.log(resultString);
-        let resultData = JSON.parse(resultString);
-
-        let sum = resultData['sum'];
-        console.log('Sum of array from Python process =', sum);
+        console.log(resultString.trim().slice(-1));
+        return res.send(JSON.stringify({"Result":resultString.trim().slice(-1)}));
     });
-
-    return res.send(JSON.stringify({"Posted":"Check Terminal"}));
 });
 
 
